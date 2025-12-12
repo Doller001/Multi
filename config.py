cdd37e69f36c2fecb2eaf6b44c2da683de5ab7c4
Minaty <saif.ali321554@gmail.com>
2025-12-12 11:31:24 +0530

    Add configuration for API and bot settings

---
+++ config.py
@@ -0,0 +1,11 @@
+API_ID = 32657209
+API_HASH = "a5d7d2497f555cd1ea9b2eefe7ffbebb"
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+
+ADMINS = [7930082566]
+
+# OPTIONAL – SHOW JOIN BUTTONS
+REQUIRED_CHANNELS = [
+    {"link": "https://t.me/+IMJWFjaXN0M3NDQ1"},
+    {"link": "https://t.me/+qT6WgjlwnrBkMWZl"}
+]
